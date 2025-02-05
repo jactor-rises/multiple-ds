bb33319041c006e94b3984e6404b9a758c79d2f4
Tor Egil Jacobsen <jactor-rises@users.noreply.github.com>
2025-02-05 09:34:57 +0100

    bump kotlin 1.9.25 -> 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.10"
 
-    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.jpa") version "2.1.10"
 }
 
 group = "com.github.jactor.rises"
